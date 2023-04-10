54923d552301a9fe1e8490eee4aed4832eb87d01
wu yuezhou <wyz18405583620@gmail.com>
2023-04-10 12:56:47 +0800

    课程查询接口测试和post工具

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -0,0 +1,24 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+
+/**
+ * @description: 课程基本信息管理业务接口
+ * @date 2023/4/10 11:11
+ */
+public interface CourseBaseInfoService {
+
+    /*
+     * @description 课程查询接口
+     * @param pageParams 分页参数
+     * @param queryCourseParamsDto 条件条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.content.model.po.CourseBase>
+     * @author Mr.M
+     * @date 2022/9/6 21:44
+     */
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+
+}
