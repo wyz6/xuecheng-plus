6b998ed025a8f2e183bf5d21820568cc39c70f1a
wu yuezhou <wyz18405583620@gmail.com>
2023-04-20 18:51:04 +0800

    上传图片

--- xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/main/java/com/xuecheng/media/service/MediaFileService.java
@@ -3,6 +3,8 @@ package com.xuecheng.media.service;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.media.model.dto.QueryMediaParamsDto;
+import com.xuecheng.media.model.dto.UploadFileParamsDto;
+import com.xuecheng.media.model.dto.UploadFileResultDto;
 import com.xuecheng.media.model.po.MediaFiles;
 
 /**
@@ -13,15 +15,37 @@ import com.xuecheng.media.model.po.MediaFiles;
  */
 public interface MediaFileService {
 
- /**
-  * @param pageParams          分页参数
-  * @param queryMediaParamsDto 查询条件
-  * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
-  * @description 媒资文件查询方法
-  * @author Mr.M
-  * @date 2022/9/10 8:57
-  */
- public PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
+    /**
+     * @param pageParams          分页参数
+     * @param queryMediaParamsDto 查询条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.media.model.po.MediaFiles>
+     * @description 媒资文件查询方法
+     * @author Mr.M
+     * @date 2022/9/10 8:57
+     */
+    PageResult<MediaFiles> queryMediaFiels(Long companyId, PageParams pageParams, QueryMediaParamsDto queryMediaParamsDto);
 
+    /**
+     * 上传文件
+     *
+     * @param companyId           机构id
+     * @param uploadFileParamsDto 上传文件信息
+     * @param localFilePath       文件磁盘路径
+     * @return 文件信息
+     */
+    UploadFileResultDto uploadFile(Long companyId, UploadFileParamsDto uploadFileParamsDto, String localFilePath);
+
+    /**
+     * @param companyId           机构id
+     * @param fileMd5             文件md5值
+     * @param uploadFileParamsDto 上传文件的信息
+     * @param bucket              桶
+     * @param objectName          对象名称
+     * @return com.xuecheng.media.model.po.MediaFiles
+     * @description 将文件信息添加到文件表
+     * @author Mr.M
+     * @date 2022/10/12 21:22
+     */
+    MediaFiles addMediaFilesToDb(Long companyId, String fileMd5, UploadFileParamsDto uploadFileParamsDto, String bucket, String objectName);
 
 }
---
+++ xuecheng-plus-media/xuecheng-plus-media-service/src/test/java/com/xuecheng/media/MinioTest.java
@@ -0,0 +1,86 @@
+package com.xuecheng.media;
+
+import com.j256.simplemagic.ContentInfo;
+import com.j256.simplemagic.ContentInfoUtil;
+import io.minio.GetObjectArgs;
+import io.minio.MinioClient;
+import io.minio.RemoveObjectArgs;
+import io.minio.UploadObjectArgs;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.compress.utils.IOUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilterInputStream;
+
+/**
+ * @author Wu-yuezhou
+ * @description 测试Minio
+ */
+public class MinioTest {
+    static MinioClient minioClient =
+            MinioClient.builder()
+                    .endpoint("http://192.168.101.65:9000")
+                    .credentials("minioadmin", "minioadmin")
+                    .build();
+
+    // 测试上传文件
+    @Test
+    public void upload() {
+        //根据扩展名取出mimeType
+        ContentInfo extensionMatch = ContentInfoUtil.findExtensionMatch(".mp4");
+        String mimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;//通用mimeType，字节流
+        if (extensionMatch != null) {
+            mimeType = extensionMatch.getMimeType();
+        }
+        try {
+            UploadObjectArgs testbucket = UploadObjectArgs.builder()
+                    .bucket("testbucket")
+//                    .object("test001.mp4")
+                    .object("001/test001.mp4")//添加子目录
+                    .filename("D:\\develop\\upload\\1mp4.temp")
+                    .contentType(mimeType)//默认根据扩展名确定文件内容类型，也可以指定
+                    .build();
+            minioClient.uploadObject(testbucket);
+            System.out.println("上传成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("上传失败");
+        }
+
+    }
+
+    // 测试删除文件
+    @Test
+    public void delete() {
+        try {
+            minioClient.removeObject(
+                    RemoveObjectArgs.builder().bucket("testbucket").object("001/test001.mp4").build());
+            System.out.println("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("删除失败");
+        }
+    }
+
+    //查询文件
+    @Test
+    public void getFile() throws Exception {
+        GetObjectArgs getObjectArgs = GetObjectArgs.builder().bucket("testbucket").object("test001.mp4").build();
+        FilterInputStream inputStream = minioClient.getObject(getObjectArgs);
+        // 指定输出流
+        FileOutputStream outputStream = new FileOutputStream(new File("D:\\develop\\upload\\1_2.mp4"));
+        IOUtils.copy(inputStream, outputStream);
+        //校验文件的完整性对文件的内容进行md5
+        FileInputStream fileInputStream1 = new FileInputStream(new File("D:\\develop\\upload\\1.mp4"));
+        String source_md5 = DigestUtils.md5Hex(fileInputStream1);
+        FileInputStream fileInputStream = new FileInputStream(new File("D:\\develop\\upload\\1a.mp4"));
+        String local_md5 = DigestUtils.md5Hex(fileInputStream);
+        if (source_md5.equals(local_md5)) {
+            System.out.println("下载成功");
+        }
+    }
+}
